d6905dc51871fb935e9edcdfec20b2ead1587821
Alex <alex.d.chapman@outlook.com>
2024-06-13 20:52:56 +0100

    Added responsive look, need to add responsive functionality and burger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,17 +7,17 @@ import {NavLink} from "react-router-dom";
 
 // ---- Navigation ----
 const Navigation = () => {
+
   return (
     <div className="container">
-      <div className="navbar">
+      <nav className="navbar">
         <div className="navbar-brand">Alex Chapman</div>
-        <div className="navbar-links">
+        <div className='navbar-links'>
           <NavLink className={({ isActive }) => `navbar-links-item ${isActive ? 'active' : ''}`} to="/">About</NavLink>
           <NavLink className={({ isActive }) => `navbar-links-item ${isActive ? 'active' : ''}`} to="/projects">Projects</NavLink>
         </div>
-      </div>
+      </nav>
     </div>
-
   );
 }
 
