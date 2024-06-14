016c2e314b035e0dce80bbddda174b36b95bdeac
Alex <alex.d.chapman@outlook.com>
2024-06-14 12:21:45 +0100

    added icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,19 @@
+import LinkedIn from "../icons/LinkedIn";
+import GitHub from "../icons/GitHub";
+import Mail from "../icons/Mail";
+import YoutTube from "../icons/YoutTube";
+
 const Footer = () => {
   return (
-    <h1>Footer!</h1>
+    <div>
+      <h1>Footer!</h1>
+      <LinkedIn height={"50px"}></LinkedIn>
+      <GitHub height={"50px"}></GitHub>
+      <Mail height={"50px"}></Mail>
+      <YoutTube height={"50px"}></YoutTube>
+    </div>
+
+
   );
 }
 
