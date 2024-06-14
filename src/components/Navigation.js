b307ba95c43cf9c0d4a61279f5e43b9e3d9988e7
Alex <alex.d.chapman@outlook.com>
2024-06-14 20:41:54 +0100

    Implemented basic hamburger menu with animations

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,14 +5,22 @@ import './Navigation.scss';
 // Routing
 import {NavLink} from "react-router-dom";
 
+// Burger menu
+import Hamburger from 'hamburger-react'
+import {useState} from "react";
+
 // ---- Navigation ----
 const Navigation = () => {
+  const [isOpen, setOpen] = useState(false)
 
   return (
     <div className="container">
       <nav className="navbar">
-        <div className="navbar-brand">Alex Chapman</div>
-        <div className='navbar-links'>
+        <div className='navbar-title-burger'>
+          <h1 className="navbar-brand">Alex Chapman</h1>
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </div>
+        <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
           <NavLink className={({ isActive }) => `navbar-links-item ${isActive ? 'active' : ''}`} to="/">About</NavLink>
           <NavLink className={({ isActive }) => `navbar-links-item ${isActive ? 'active' : ''}`} to="/projects">Projects</NavLink>
         </div>
