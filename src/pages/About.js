94f9d9dff77dac1a640554e52a52f779abd26a6c
Alex <alex.d.chapman@outlook.com>
2024-06-15 17:44:24 +0100

    Added Biography Content

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,27 @@ import './About.scss';
 // Components
 import Layout from '../components/Layout'
 
+// Images
+import bio_pic from '../images/Alex_holiday.png'
+
 function About() {
   return (
     <Layout>
-      <h1>About!</h1>
+      <div className='biography'>
+        <div className='biography-text'>
+          <h1 className='biography-text-title'>Biography</h1>
+          <h2 className='biography-text-subTitle'>Masters Computer Science Student at The University of Sheffield</h2>
+          <p className='biography-text-paragraph'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+          <p className='biography-text-paragraph'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet venenatis urna cursus eget nunc scelerisque viverra. Sit amet facilisis magna etiam tempor. Euismod elementum nisi quis eleifend. Mauris augue neque gravida in fermentum. In metus vulputate eu scelerisque felis imperdiet proin. At auctor urna nunc id cursus metus aliquam eleifend. Enim blandit volutpat maecenas volutpat. Sit amet nisl purus in mollis nunc sed id. Sit amet nisl purus in mollis nunc sed id. Sit amet nisl purus in mollis nunc sed id.
+          </p>
+        </div>
+        <div className='biography-image'>
+          <img className='biography-image-image' src={bio_pic} alt="Alex_Holiday" />
+          <div className='biography-image-blank'></div>
+        </div>
+      </div>
     </Layout>
   );
 }
