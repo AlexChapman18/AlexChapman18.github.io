688cc325e2833fbf157ba783b647491653100b01
Alex Chapman <achapman5@sheffield.ac.uk>
2024-06-24 15:36:54 +0100

    started bio and Experience section

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,7 @@ function About() {
             <h1 className='biography-text-title section-title'>Biography</h1>
             <h2 className='biography-text-subTitle section-subtitle'>Masters Computer Science Student at The University of Sheffield</h2>
             <p className='biography-text-paragraph section-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+              I’m a 4th year Masters Computer Science student at the University of Sheffield, passionate in software development and live events production. Outside of my degree, I have undertaken a variety of both personal and group projects, applying and improving my knowledge. Some examples can be seen on my projects page.</p>
             <p className='biography-text-paragraph section-text'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet venenatis urna cursus eget nunc scelerisque viverra. Sit amet facilisis magna etiam tempor. Euismod elementum nisi quis eleifend. Mauris augue neque gravida in fermentum. In metus vulputate eu scelerisque felis imperdiet proin. At auctor urna nunc id cursus metus aliquam eleifend. Enim blandit volutpat maecenas volutpat. Sit amet nisl purus in mollis nunc sed id. Sit amet nisl purus in mollis nunc sed id. Sit amet nisl purus in mollis nunc sed id.
             </p>
@@ -68,7 +68,8 @@ function About() {
         <hr className='lineSeparator'/>
         <div className='experience'>
           <h1 className='experience-title section-title'>Experience</h1>
-          <p className='experience-text section-text'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim</p>
+          <p className='experience-text section-text'>
+          Having worked in the live events industry for the past 4 years, I have gained significant hands-on experience being both a stage manager and team member in excess of 100 individual events, ranging from gigs and club nights to awards ceremonies at the Sheffield Uni. External to the university, I have also worked as the head lighting engineer for a 3-day rock festival at the O2 Academy Sheffield.          </p>
         </div>
         <hr className='lineSeparator'/>
         <div className='skills'>
