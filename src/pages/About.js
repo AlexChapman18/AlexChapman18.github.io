9240387d0edb04db947f09d9f3dafefdac373f37
Alex Chapman <alex.d.chapman@outlook.com>
2024-12-24 11:52:43 +0000

    Added Skiing to Interests

--- src/pages/About.js
+++ src/pages/About.js
@@ -38,6 +38,7 @@ function About() {
               <li>Programming Projects</li>
               <li>Gaming</li>
               <li>Geocaching</li>
+              <li>Skiing</li>
             </ul>
           </div>
           <hr className='lineSeparator IE-separator'/>
@@ -74,8 +75,7 @@ function About() {
         <hr className='lineSeparator'/>
         <div className='skills'>
           <h1 className='skills-title section-title'>Skills</h1>
-          {/* <p className='skills-text section-text'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim</p> */}
-          <p className='skills-text section-text'>My skills include being a team leader, having lead a software development team in several course modules to success.   </p>
+          <p className='skills-text section-text'>My skills: Advanced Git, </p>
         </div>
         <hr className='lineSeparator bottom-seperator'/>
       </div>
