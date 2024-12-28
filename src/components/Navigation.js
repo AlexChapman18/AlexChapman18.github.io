7d0d29b1d29f9732b36d39f4db746ebb3dbc120d
Alex Chapman <alex.d.chapman@outlook.com>
2024-12-28 17:27:13 +0000

    Linted Navigation js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,31 @@
 // ---- Imports ----
 
 // Routing
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Burger menu
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Nav, Navbar } from "react-bootstrap";
 
 // ---- Navigation ----
-const Navigation = () => {
-  return (
-    <Navbar expand="lg py-0" className="bg-body-tertiary">
-      <div className="container">
-        <Navbar.Brand className="fs-1 fw-bold text-primary" href="#">Alex Chapman</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="navbar-nav fs-3 ms-auto" navbarScroll>
-            <NavLink className="nav-link" to="/">About</NavLink>
-            <NavLink className="nav-link" to="/projects">Projects</NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-  );
-}
+const Navigation = () => (
+  <Navbar expand="lg" className="bg-body-tertiary py-0">
+    <div className="container">
+      <Navbar.Brand className="fs-1 fw-bold text-primary" href="#">
+        Alex Chapman
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav className="fs-3 ms-auto" navbarScroll>
+          <NavLink className="nav-link" to="/">
+            About
+          </NavLink>
+          <NavLink className="nav-link" to="/projects">
+            Projects
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </div>
+  </Navbar>
+);
 
-export default Navigation
+export default Navigation;
