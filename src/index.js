68f0e4767125d5ec31798512645296d674f23be0
Alex Chapman <alex.d.chapman@outlook.com>
2025-01-04 17:25:15 +0000

    Removed redundant index.css, Linted and removed redundant code from index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import "./styles/overrides.scss";
+import "./styles/global.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
-  </React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
